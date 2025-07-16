fa5a1dc1573203c15c009b5bc1a6f2464c9fdc44
navonsanjuni <navonsanjuni178@gmail.com>
2025-07-16 07:22:28 +0530

    Create Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import {Toaster} from "sonner"
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Route>
         <Route>{/*Admin Layout*/}</Route>
       </Routes>
